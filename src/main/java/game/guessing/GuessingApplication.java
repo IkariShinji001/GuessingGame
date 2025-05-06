4610026f0b16b166b4d6c321d72dc51f87b1f606
thanh-duy <thanh-duy@system-exe.com>
2025-05-06 09:13:03 +0700

    init

---
+++ src/main/java/game/guessing/GuessingApplication.java
@@ -0,0 +1,13 @@
+package game.guessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/game/guessing/GuessingApplicationTests.java
@@ -0,0 +1,13 @@
+package game.guessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
